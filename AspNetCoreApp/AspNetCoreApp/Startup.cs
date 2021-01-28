796624ff072567622c3f31c18d593f3208dc7dbf
Anton Piskunov <antonpis@microsoft.com>
2021-01-28 08:03:36 -0800

    Add AI dependency

--- AspNetCoreApp/AspNetCoreApp/Startup.cs
+++ AspNetCoreApp/AspNetCoreApp/Startup.cs
@@ -24,6 +24,7 @@ namespace AspNetCoreApp
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddRazorPages();
+            services.AddApplicationInsightsTelemetry(Configuration["APPINSIGHTS_CONNECTIONSTRING"]);
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
